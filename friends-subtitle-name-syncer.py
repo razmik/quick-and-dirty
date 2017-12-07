a410b8d9071f23cb55a0f9a19cc6f459e318a075
razmik <razmik89@gmail.com>
2017-12-07 11:13:09 +1100

    first commit - subtitle syncer

---
+++ friends-subtitle-name-syncer.py
@@ -0,0 +1,31 @@
+from os import listdir, rename
+from os.path import isfile, join
+
+folder_name = 'C:/Users\pc\Desktop/Friends S03 720p BluRay x264 PaHe.in/'.replace('\\', '/')
+
+"""
+video: Friends.S03E02.720p.BluRay.x264.195MB-PAHE.in.mkv
+srt: Friends.S03E02.720p.BluRay.x264-PSYCHD.srt
+"""
+
+if __name__ == '__main__':
+
+    files = [f for f in listdir(folder_name) if isfile(join(folder_name, f))]
+
+    srt_dict = {}
+    video_file_name = ''
+
+    # Get the video filename
+    for file in files:
+        filename_components = file.split('.')
+        if 'mkv' in filename_components[-1]:
+            video_file_name = filename_components[:-1]
+            break
+
+    # Loop through the srt files and rename to video filename
+    for file in files:
+        filename_components = file.split('.')
+        if 'srt' in filename_components[-1]:
+            new_filename = ''.join(str(e) + '.' for e in filename_components[:4]) + ''.join(
+                str(f) + '.' for f in video_file_name[4:]) + 'srt'
+            rename(folder_name + file, folder_name + new_filename)
